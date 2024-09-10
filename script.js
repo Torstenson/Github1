bc62da7862900dda4104a74d1c486a85cc2d9826
Torstenson <jennie@torstensons.se>
2024-09-10 10:59:36 +0200

    3

---
+++ script.js
@@ -0,0 +1,3 @@
+document.addEventListener('DOMContentLoaded', function() {
+    alert('Välkommen till vår hemsida! Här kan du läsa mer om våra tjänster och hur vi kan hjälpa dig eller ert parförhållande att växa.');
+});
